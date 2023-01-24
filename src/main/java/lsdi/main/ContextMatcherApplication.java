8e03a38b5f5708b23e39635a24186e5cb3a92b79
Vitor Santos <vitoralannl@gmail.com>
2023-01-24 18:13:40 -0300

    config (@general) Configurações iniciais do projeto

--- src/main/java/lsdi/main/ContextMatcherApplication.java
+++ src/main/java/lsdi/main/ContextMatcherApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContextMatcherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContextMatcherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContextMatcherApplication.class, args);
+    }
 
 }
--- src/test/java/lsdi/main/ContextMatcherApplicationTests.java
+++ src/test/java/lsdi/main/ContextMatcherApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ContextMatcherApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
