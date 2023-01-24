1be14c80a900ae6cb7e0a2089a03c7883fbc6d0b
Vitor Santos <vitoralannl@gmail.com>
2023-01-24 18:10:32 -0300

    Initial commit

---
+++ src/main/java/lsdi/main/ContextMatcherApplication.java
@@ -0,0 +1,13 @@
+package lsdi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsdi/main/ContextMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package lsdi.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
